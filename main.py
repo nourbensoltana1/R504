b3b2d42b91aea5a34b30fc56944befb14f5b409f
nourbensoltana1 <nour.bensoltana@etudiant-isi.utm.tn>
2025-10-08 16:47:50 +0200

    feat: ajout fichier principal main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from utils import afficher_menu, demander_choix
+from temperature import convertir_temperature
+from distance import convertir_distance
+
+def main():
+    print("=== Convertisseur d'unités ===")
+
+    while True:
+        afficher_menu()
+        choix = demander_choix(["1", "2", "q"])
+
+        if choix == "1":
+            convertir_temperature()
+        elif choix == "2":
+            convertir_distance()
+        elif choix == "q":
+            print("Merci d’avoir utilisé le convertisseur !")
+            break
+
+if __name__ == "__main__":
+    main()
+
