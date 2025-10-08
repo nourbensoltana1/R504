071ee8f06af6600c1052047d5950394521606452
AMMAR Ons <12512496@r20201.iutv.univ-paris13.fr>
2025-10-08 16:29:35 +0200

    Premier commit par collaborateur

---
+++ temperature.py
@@ -0,0 +1,7 @@
+def celsius_vers_fahrenheit(c):
+	"""Convertit Celsius → Fahrenheit"""
+	return (c * 9/5) + 32
+
+def fahrenheit_vers_celsius(f):
+	"""Convertit Fahrenheit → Celsius"""
+	return (f - 32) * 5/9
