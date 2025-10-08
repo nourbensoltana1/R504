f88beb3934ec23e6f23a90fb95213215d013ddc3
nourbensoltana1 <nour.bensoltana@etudiant-isi.utm.tn>
2025-10-08 16:29:52 +0200

    feat: ajout fonction demander_choix

--- utils.py
+++ utils.py
@@ -2,3 +2,11 @@ def afficher_menu():
     print("1. Convertir une température")
     print("2. Convertir une distance")
     print("q. Quitter")
+
+def demander_choix(choix_possibles):
+    choix = input("Votre choix : ")
+    if choix not in choix_possibles:
+        print("Choix invalide")
+        return demander_choix(choix_possibles)
+    return choix
+
