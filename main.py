56088a0cb7d542a4dade0b7fa103167ee4936bdb
nourbensoltana1 <nour.bensoltana@etudiant-isi.utm.tn>
2025-10-09 21:00:12 +0200

    feat: ajout d’une option d’aide dans le menu principal

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
-from utils import afficher_menu, demander_choix, confirmer_recommencer
+from utils import afficher_menu, demander_choix, confirmer_recommencer, afficher_aide
 from temperature import convertir_temperature
 from distance import convertir_distance
+
 
 def main():
     print("Bienvenue dans notre convertisseur d’unités développé en binôme !")
 
     while True:
         afficher_menu()
-        choix = demander_choix(["1", "2", "q"])
+        choix = demander_choix(["1", "2", "h", "q"])
 
         if choix == "1":
             convertir_temperature()
         elif choix == "2":
             convertir_distance()
+        elif choix == "h":
+            afficher_aide()
         elif choix == "q":
             print("Merci d’avoir utilisé le convertisseur !")
-        elif choix == "h":
-   	    afficher_aide()
-	    break
+            break
+
+        # Confirmation de redémarrage
         if not confirmer_recommencer():
             print("Merci d’avoir utilisé le convertisseur !")
             break
+
 
 if __name__ == "__main__":
     main()
